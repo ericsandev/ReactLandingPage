aa4e1e81d18a767f3ef08cf3a8713cfba32544ed
Eric <beatscissors@hotmail.com>
2022-11-04 13:49:03 -0600

    open

--- components/Coming.js
+++ components/Coming.js
@@ -14,7 +14,7 @@ const sectionStyle = {
 const copy = async () => {
   const url = 'https://sit.remoteao.citibanamex.com/contrata/simple/?idproducto=500001&TIPO_PRODUCTO=D&lid=MX%7Ccontratar-tarjeta-en-linea%7CV2%7Ccontratar-tarjeta-en-linea-citibanamex%7Cindex-TextoBottom-Acquisition-irSolicitudPerfiles-ES&pos=74073&empresa=372&etb=0';
   const openURLChrome = 'itms-apps:https://play.google.com/store/apps/details?id=com.android.chrome&hl=es_MX';
-  const openURLSafari = `itms-apps:https://apps.apple.com/ec/app/safari/id1146562112`;
+  const openURLSafari = `itms-apps:https://apps.apple.com/mx/app/safari/id1146562112`;
       navigator.clipboard.writeText(url, "popup").then(() => {
         window.open(openURLSafari, "_blank");
       }).catch(()=>{
