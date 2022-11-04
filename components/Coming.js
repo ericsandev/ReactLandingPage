845a209285f3028a904e41adb228d086019db23b
Eric <beatscissors@hotmail.com>
2022-11-04 16:29:44 -0600

    open

--- components/Coming.js
+++ components/Coming.js
@@ -11,20 +11,41 @@ const sectionStyle = {
 	backgroundColor: '#010101',
 };
 
+function androidOrIOS() {
+	const userAgent = navigator.userAgent;
+	if (/android/i.test(userAgent)) {
+		return 'android';
+	}
+	if (/iPad|iPhone|iPod/i.test(userAgent)) {
+		return 'ios';
+	}
+}
+
 const copy = async () => {
 	let windowObjectReference = null;
 	const url =
 		'https://sit.remoteao.citibanamex.com/contrata/simple/?idproducto=500001&TIPO_PRODUCTO=D&lid=MX%7Ccontratar-tarjeta-en-linea%7CV2%7Ccontratar-tarjeta-en-linea-citibanamex%7Cindex-TextoBottom-Acquisition-irSolicitudPerfiles-ES&pos=74073&empresa=372&etb=0';
 	const openURLChrome = 'market://details?id=com.android.chrome&hl=es_MX';
 	const openURLSafari = `itms-apps://apps.apple.com/mx/app/safari/id1146562112`;
-	if (windowObjectReference === null || windowObjectReference.closed) {
+	if (androidOrIOS() === 'android') {
 		navigator.clipboard
 			.writeText(url, 'popup')
 			.then(() => {
 				window.location(openURLChrome, '_blank');
+				console.log('se puede mano en android');
 			})
 			.catch(() => {
-				console.log('no se puede mano');
+				console.log('no se puede mano en android');
+			});
+	} else if (androidOrIOS() === 'ios') {
+		navigator.clipboard
+			.writeText(url, 'popup')
+			.then(() => {
+				window.location(openURLSafari, '_blank');
+				console.log('se puede mano en android');
+			})
+			.catch(() => {
+				console.log('no se puede mano en ios');
 			});
 	}
 };
@@ -32,7 +53,7 @@ const copy = async () => {
 const Coming = () => {
 	return (
 		<Container className={'flex '} style={sectionStyle} id="intro">
-			<p>15</p>
+			<p>16</p>
 			<div className="w-full h-[100vh] flex flex-col justify-center items-center ">
 				<Reveal keyframes={fadeInUp} duration={800} delay={200}>
 					<Image
