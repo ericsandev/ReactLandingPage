f246009fa30b12136597f1284e2b258b7f02c2ff
ericsandev <ericsandev0@gmail.com>
2023-02-13 12:53:36 -0600

    update

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,42 @@
-
 import React from 'react';
 import {
-  AboutUs,
-  Button,
-  Collections,
-  Container,
-  FAQ,
-  Footer,
-  Hero,
-  Navbar,
-  Team,
-  Marketing,
-  CallTo,
-  Social,
-  Coming
+	AboutUs,
+	Button,
+	Collections,
+	Container,
+	FAQ,
+	Footer,
+	Hero,
+	Navbar,
+	Team,
+	Marketing,
+	CallTo,
+	Social,
+	Coming,
 } from '@/components';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
-  return (
-    <div className='h-full main_bg text-white overflow-hidden' id='top'>
-      <Coming />
-    </div>
-  );
+	return (
+		<div className="h-full main_bg text-white overflow-hidden" id="top">
+			<Helmet>
+				<title>Hypnofrens | Welcome 6666 NFTS Hypnofrens</title>
+				<meta
+					name="description"
+					content='Hypnofrens focuses on establishing a brand IRL through partnerships with companies around the world. In addition to a partnership with an animation studio and writers for the creation of an animated series at the same time that we are preparing the next collection called "The Sensors".'
+				/>
+			</Helmet>
+			<Navbar />
+			<Marketing />
+			<Hero />
+			<AboutUs />
+			<Collections />
+			<Team />
+			<FAQ />
+			<CallTo />
+			<Footer />
+		</div>
+	);
 };
 
 export default Home;
